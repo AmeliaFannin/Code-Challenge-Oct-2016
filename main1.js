f5d13cefba9f8c5a1a09aca6313159262720b084
Amelia Fannin <ameliafannin@Amelias-MacBook-Pro.local>
2016-10-13 14:58:07 -0400

    added question 2

--- main1.js
+++ main1.js
@@ -1,11 +1,11 @@
 // See the two tab delimited files attached:
 // products.tab = A list of product names tab delimited with categories
 // sales.tab = A list of product names tab delimited with sales amount
- 
+
 // From this data answer two questions:
 // 1. What are the top 5 categories by sales
 // 2. What is the top product by sales in category 'Candy'
- 
+
 // I added keys at the top of each TAB file so I could have keyed info. 
 // there are some problems in formatting of the data, so some values are undefined.
 
@@ -30,37 +30,63 @@ function getTopSalesCategories(length) {
 	var salesByCategory = [];
 	
   var salesEntries = productSalesData.map(function(entry) {
-      return {'category': lookUpCategory(entry.product), 'sales': parseFloat(entry.sales)};
-    });
+    return {'category': lookUpCategory(entry.product), 'sales': parseFloat(entry.sales)};
+  });
 
-	salesEntries.reduce(function(a, b) {
-      return a.concat(b.category);
-    }, [])
-    .filter(function(elem, index, self) {
-      return index === self.indexOf(elem);
-    })
-    .map(function(cat) {
-		  var total = salesEntries.filter(function(i) {
-			  return i.category === cat;
-		  })
-      .reduce(function(a, b){
-			  return a + b.sales;
-		  }, 0);
+  salesEntries.reduce(function(a, b) {
+    return a.concat(b.category);
+  }, [])
+  .filter(function(elem, index, self) {
+    return index === self.indexOf(elem);
+  })
+  .map(function(cat) {
+    var total = salesEntries.filter(function(i) {
+     return i.category === cat;
+   })
+    .reduce(function(a, b){
+     return a + b.sales;
+   }, 0);
 
-		salesByCategory.push({'category': cat, 'sales': total});
-	});
-	
-	return salesByCategory.sort(function(a, b) {
-      if (a.sales > b.sales) {
-        return -1;
-      }
-      if (a.sales < b.sales) {
-        return 1;
-      }
-      if (a.sales === b.sales) {
-        return 0;
-      }
-    }).slice(0, length);;
+    salesByCategory.push({'category': cat, 'sales': total});
+  });
+
+  return salesByCategory.sort(function(a, b) {
+    if (a.sales > b.sales) {
+      return -1;
+    }
+    if (a.sales < b.sales) {
+      return 1;
+    }
+    if (a.sales === b.sales) {
+      return 0;
+    }
+  }).slice(0, length);;
 }
 
-console.log(getTopSalesCategories(5));
+function getTopSellingProduct(category){
+  var topSeller = [];
+
+  productCategoryData.filter(function(i) {
+    return i.category === category;
+  })
+  .reduce(function(a, b) {
+    return a.concat(b.product);
+  }, [])
+  .map(function(product){
+    var total = productSalesData.filter(function(j) {
+      return j.product === product;
+    })
+    .reduce(function(a, b){
+      return a + parseFloat(b.sales);
+    }, 0);
+
+    if (topSeller.length < 1 || total > parseFloat(topSeller.sales)) {
+      topSeller.splice(0, 1, {'product': product, 'sales': total});
+    }
+  });
+
+  return topSeller;
+}
+
+console.log(getTopSellingProduct('Candy'));
+// console.log(getTopSalesCategories(5));
